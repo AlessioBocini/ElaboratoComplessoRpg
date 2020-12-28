0caad9b8cf41256a9a6f0013c8c0b1e0768cf242
AlessioB <38136763+AlessioBocini@users.noreply.github.com>
2020-12-28 13:28:13 +0100

    NemicoComune.h - Delete
    + rimosso in quanto progettualmente scorretto

--- ConsoleApplication1/NemicoComune.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Nemico.h"
-class NemicoComune : public Nemico {
-public:
-	NemicoComune(std::string nome, int forza, int vitalita, int livello, float velocita, sf::Vector2f pos, unsigned int id, SharedContext* context, std::string entityType) : Nemico(nome, forza, vitalita, livello, velocita, pos, id, context, entityType) {}
-	~NemicoComune() {}
-
-
-};
