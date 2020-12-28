8d90409df94def544d23ef25d812abbafc1ff206
AlessioB <38136763+AlessioBocini@users.noreply.github.com>
2020-12-28 13:16:40 +0100

    Entita - Commit
    + Interazione (r)
    + Collisione con entita
    + Attacco (f)
    + Rigenerazione vita e stamina

--- ConsoleApplication1/Entita.cpp
+++ ConsoleApplication1/Entita.cpp
@@ -1,8 +1,8 @@
 #include "Entita.h"
 #include "GameManager.h"
 #include <iostream>
-void Entita::CheckCollisions(SharedContext* context) {
-	Mondo* world = context->gameMap;
+void Entita::CheckCollisions(/*SharedContext* context*/) {
+	Mondo* world = context.gameMap;
 	unsigned int tileSize = world->GetTileSize(); //ottengo la dimensione del tile = 32x32 => solo 32.
 
 	int fromX = (int)floor(rectColl.left / tileSize); //rettangolo di collisione dell'entità parte sinistra
@@ -18,37 +18,59 @@ void Entita::CheckCollisions(SharedContext* context) {
 		for (int y = fromY; y <= toY; ++y) {
 			//faccio due for innestati, in quanto controllo tutte le coordinate in quell'area (e così i rispettivi tile).
 
-			Tile* tile = world->GetTileByCoords(x, y); // ottengo il tile 
+			Tile* tile = world->getActualMap().GetTileByCoords(x, y); // ottengo il tile 
 
 
 			if (!tile) continue; //se non esiste, skip
 			if (!tile->properties->blocked && !tile->teleport)continue; //se non è bloccato e non è teletrasporto...skip
 			//(in quanto significa che è un tile effettivo, ma non interessante)
 
-
-			sf::FloatRect tileBounds = sf::FloatRect(x * tileSize, y * tileSize, tileSize, tileSize);
-			// ottengo i boundaries.
-			sf::FloatRect intersection;
-
-			rectColl.intersects(tileBounds, intersection);
-			//questa funzione inserisce in "intersection" il valore di ritorno sull'intersezione.
-
-			float area = intersection.width * intersection.height; //calcolo l'area d'intersezione.
-			CollisionElement e(area, tile->properties, tileBounds); //creo un nuovo CollisionElement.
-			colls.emplace_back(e); //lo inserisco nelle collisioni avvenute.
-
 			if (tile->teleport && entitytype == "Player") {
 				//Se il tile è di teletrasporto esegue questa parte
+				if (Interacting) {
+	
+					sf::Clock clock = sf::Clock();
+					if (tile->doorPos != nullptr) {
+						float posx = tile->doorPos->x,
+							  posy = tile->doorPos->y;
 
-				world->EraseMap(); //elimina la mappa attuale
-				std::string lastMap = world->GetPrevMap(); //ottengo quale era la mappa precedente
-				std::cout << tile->nextMap << std::endl;
-				world->LoadMap(tile->nextMap, false);//carico la mappa successiva (definita nel tile colliso)
-				world->SetPlayerStartpoint(tile->teleportPos);//definisco il nuovo spawnpoint 
-				context->gameManager->SetPlayerStart(world->GetPlayerStartpoint()); // lo memorizzo anche nel gameManager
-				world->SetPrevMap(tile->nextMap);//setto quale è la mappa precedente.
-
+						Arredamento* decoration = world->getActualMap().GetDecorationByCoords(posx, posy);
+						if (decoration != nullptr) { 
+							Animator* decoAnimpg = decoration->GetAnimpg();
+							decoAnimpg->SwitchAnimation("animation1");
+							decoration->PrepareTeleport(tile);
+						}
+						else {
+							//caso di debug
+							std::cout << "Errore! - ho coordinate porta, non ho la porta" << std::endl;
+							// è impossibile che non ci sia la porta se ho le sue coordinate
+						}
+					}
+					else {
+						context.gameManager->SetPlayerStart(tile->teleportPos);
+						world->SetPrevMap(tile->nextMap);//setto quale è la mappa precedente.
+						world->ChangeActualMap(tile->nextMap);
+						world->getActualMap().SetPlayerStartpoint(tile->teleportPos);//definisco il nuovo spawnpoint
+					}
+					
+					
+				}
 			}
+			else {
+				sf::FloatRect tileBounds = sf::FloatRect(x * tileSize, y * tileSize, tileSize, tileSize);
+				// ottengo i boundaries.
+				sf::FloatRect intersection;
+
+				rectColl.intersects(tileBounds, intersection);
+				//questa funzione inserisce in "intersection" il valore di ritorno sull'intersezione.
+
+				float area = intersection.width * intersection.height; //calcolo l'area d'intersezione.
+				CollisionElement e(area, tile->properties, tileBounds); //creo un nuovo CollisionElement.
+				colls.emplace_back(e); //lo inserisco nelle collisioni avvenute.
+			}
+			
+
+			
 		}
 	}
 }
@@ -59,18 +81,20 @@ void Entita::setSpawnPoint(sf::Vector2f pos) {
 	position.y = pos.y * 32;
 	sprite.setPosition(pos.x * 32, pos.y * 32);
 }
-void Entita::ResolveCollisions(SharedContext* context) {
+void Entita::ResolveCollisions(/*SharedContext* context*/) {
 	isblockedA = false, isblockedD = false, isblockedS = false, isblockedW = false;
 
 	//se ci sono state collissioni
 	if (!colls.empty()) {
 		std::sort(colls.begin(), colls.end(), CollisionElement::SortCollisions); //faccio il sort delle aree 
 		// (una collisione dove l'intersezione è maggiore, ha più influenza e verrà applicata)
-		Mondo* world = context->gameMap;
+		Mondo* world = context.gameMap;
 		unsigned int tileSize = world->GetTileSize();
-		ResolveCollisionEntity(context);
+		ResolveCollisionEntity(/*context*/);
+		
 		//si va a controllare e valutare cosa fare per ciascuna collissione
-		for (auto& it : colls) {
+
+		for (CollisionElement& it : colls) {
 
 			if (!rectColl.intersects(it.tilebounds)) {
 				// diciamo è un caso di debug, nel quale nonostante si tratta di un caso di collissione con l'entità, 
@@ -97,14 +121,19 @@ void Entita::ResolveCollisions(SharedContext* context) {
 					resolve = (it.tilebounds.left + tileSize) - rectColl.left; // il tileSize corrisponde al width del tileBound.
 					// definisco la posizione corrispondente al bordo sinistro del tile
 					isblockedD = true;
+					if (entitytype == "Nemico1") {
+						animpg.SwitchAnimation("animationD");
+					}
 				}
 				else {
 					//altrimenti sta collidendo a sinistra.
 					resolve = -((rectColl.left + rectColl.width) - it.tilebounds.left);
 					// definisco la posizione corrispondente al bordo destro del tile
 					isblockedA = true;
+					if (entitytype == "Nemico1") {
+						animpg.SwitchAnimation("animationA");
+					}
 				}
-
 				Movimento(resolve, 0); //sposto l'entità
 			}
 			else {
@@ -113,24 +142,30 @@ void Entita::ResolveCollisions(SharedContext* context) {
 					resolve = (it.tilebounds.top + tileSize) - rectColl.top;
 					// definisco la posizione corrispondente al bordo in alto del tile 
 					isblockedS = true;
+
+					if (entitytype == "Nemico1") {
+						animpg.SwitchAnimation("animationS");
+					}
 				}
 				else {
 					//altrimenti sta collidendo in alto.
 					resolve = -((rectColl.top + rectColl.height) - it.tilebounds.top);
 					// definisco la posizione corrispondente al bordo in basso del tile
 					isblockedW = true;
+					if (entitytype == "Nemico1") {
+						animpg.SwitchAnimation("animationW");
+					}
 				}
 				Movimento(0, resolve); //sposto l'entità
 			}
 		}
 		colls.clear(); //elimino la collissione
 	}
-
+	Attacking = false, Interacting = false;
 }
-void Entita::CollisionEntity(Entita* ent, SharedContext* context) {
-	Mondo* world = context->gameMap;
+void Entita::CollisionEntity(Entita* ent/*, SharedContext* context*/) {
+	Mondo* world = context.gameMap;
 	unsigned int tileSize = world->GetTileSize(); //ottengo la dimensione del tile = 32x32 => solo 32.
-	auto ps = ent;
 
 	int fromX = (int)floor(rectColl.left / tileSize); //rettangolo di collisione dell'entità parte sinistra
 	int toX = (int)floor((rectColl.left + rectColl.width) / tileSize);//rettangolo di collisione dell'entità parte sinistra + larghezza
@@ -138,15 +173,13 @@ void Entita::CollisionEntity(Entita* ent, SharedContext* context) {
 	int toY = (int)floor((rectColl.top + rectColl.height) / tileSize);//rettangolo di collisione dell'entità parte in alto + altezza
 	//( divido per il tilesize perché nei valori di top e height, width e left ci sono pixel )
 
-	int fromXEnt = (int)floor(ps->GetCollRect().left / tileSize); //rettangolo di collisione dell'entità parte sinistra
-	int toXEnt = (int)floor((ps->GetCollRect().left + ps->GetCollRect().width) / tileSize);//rettangolo di collisione dell'entità parte sinistra + larghezza
-	int fromYEnt = (int)floor(ps->GetCollRect().top / tileSize);//rettangolo di collisione dell'entità parte in alto
-	int toYEnt = (int)floor((ps->GetCollRect().top + ps->GetCollRect().height) / tileSize);//rettangolo di collisione dell'entità parte in alto + altezza
+	int fromXEnt = (int)floor(ent->GetCollRect().left / tileSize); //rettangolo di collisione dell'entità parte sinistra
+	int toXEnt = (int)floor((ent->GetCollRect().left + ent->GetCollRect().width) / tileSize);//rettangolo di collisione dell'entità parte sinistra + larghezza
+	int fromYEnt = (int)floor(ent->GetCollRect().top / tileSize);//rettangolo di collisione dell'entità parte in alto
+	int toYEnt = (int)floor((ent->GetCollRect().top + ent->GetCollRect().height) / tileSize);//rettangolo di collisione dell'entità parte in alto + altezza
 	//( divido per il tilesize perché nei valori di top e height, width e left ci sono pixel )
 	// determino da che punto a che punto sull'asse delle x devo fare il controllo.
 	// determino da che punto a che punto sull'asse delle y devo fare il controllo.
-	//std::cout << "Player : " << fromXEnt << " " << toXEnt << " " << fromYEnt << " " << toYEnt << std::endl;
-	//std::cout << "Enemy : " << fromX << " " << toX << " " << fromY << " " << toY << std::endl;
 	for (int x = fromX; x <= toX; ++x) {
 		for (int y = fromY; y <= toY; ++y) {
 
@@ -161,69 +194,272 @@ void Entita::CollisionEntity(Entita* ent, SharedContext* context) {
 
 					float area = intersection.width * intersection.height; //calcolo l'area d'intersezione.
 					Tile* tile = new Tile();
-					tile->properties = new TileInfo(context);
+					tile->properties = new TileInfo(&context);
 					tile->properties->blocked = true;
 					CollisionElement e(area, tile->properties, tileBounds, true); //creo un nuovo CollisionElement.
 					colls.emplace_back(e); //lo inserisco nelle collisioni avvenute.
-					this->entitiescollided.push_back(ent);
+					this->entitiescollided.push_back(ent);// aggiungo ent alle entità collise
+				}
+				else {
 				}
 			}
+			else {
+			}
+			
 		}
 	}
 }
-void Entita::ResolveCollisionEntity(SharedContext* context) {
-	Mondo* world = context->gameMap;
+void Entita::ResolveCollisionEntity(/*SharedContext* context*/) {
+	Mondo* world = context.gameMap;
 	unsigned int tileSize = world->GetTileSize();
-	for (auto& it : colls) {
-		if (!it.entity)
+
+	sf::Vector2f thisPosition = GetSprite().getPosition();
+	for (CollisionElement& it : colls) { //controllo tutte le collisioni
+		if (!it.entity) //se è
 			continue;
 
-		for(auto&ent: entitiescollided){
+		for(Entita* &ent: entitiescollided){
 			//spostamento
 
-				// da vicino l'entità all'estremo orizzontale - dall'estremo orizzontale a vinico il centro del tile (destra o sinistra dipende dal fatto che diffx sia > o < di 0)
-			float diffx = (rectColl.left + (rectColl.width / 2)) - (it.tilebounds.left + (it.tilebounds.width / 2));
-			// da vicino l'entità all'estremo verticale - dall'estremo verticale a vinico il centro del tile (alto o basso dipende dal fatto che diffx sia < o > di 0)
-			float diffy = (rectColl.top + (rectColl.height / 2) - (it.tilebounds.top + (it.tilebounds.height / 2)));
-			float segmentx = (ent->GetPosition().x - GetPosition().x)/tileSize;
-			float segmenty = (ent->GetPosition().y - GetPosition().y)/tileSize;
+			sf::Vector2f entPos = ent->GetSprite().getPosition();
+			float segmentx = (entPos.x - thisPosition.x)/tileSize;  //distanza tra ent e questa entità asse X
+			float segmenty = (entPos.y - thisPosition.y)/tileSize; //distanza tra ent e questa entità asse Y
+	
+			if (Attacking) {
 
-			float resolve = 0;
-			
-			float resolverx = segmentx + (diffx / tileSize);
-			float resolvery = segmenty - (diffy / tileSize);
-			
-			// è importante tuttavia reimpostare la posizione dell'entità nel momento in cui essa collide, al bordo corrispondente.
-			//altrimenti rimane incastrata nel tile.
-			//inoltre è evidente che se mi muovo orizzontalmente il diffy sarà sempre minore di diffx
-			//stessa cosa ma viceversa se mi muovo verticalmente
-				if (abs(diffx) > abs(diffy)) {
+				sf::Time elapsed = clockAttack.getElapsedTime();
 
-					if (this->D && resolverx > 0){
-						//l'entità sta collidendo sulla destra.
-						isblockedD = true;
+				if (elapsed.asSeconds() > cooldownAutoAttack) {
+
+					if (segmentx > 0 && this->lastDirection == 'D') { 
+						//caso in cui il nemico è alla mia destra e mi muovo a destra
+						if (Attacco(ent, SkillAttack)) {
+						}
+						clockAttack.restart();
 					}
-					else if (this->A && resolverx <= 0) {
-						isblockedA = true;
+					else if (segmentx <= 0 && this->lastDirection == 'A') {
+						//caso in cui il nemico è alla mia sinistra e mi muovo a sinistra
+						if (Attacco(ent, SkillAttack)) {
+						}
+						clockAttack.restart();
 					}
-					else {
-						//situazione strana + diagonale
+					else if (segmenty > 0 && this->lastDirection == 'S') {
+						//caso in cui il nemico è a sud e mi muovo in basso
+						if (Attacco(ent, SkillAttack)) {
+						}
+						clockAttack.restart();
+					}
+					else if (segmenty <= 0 && this->lastDirection == 'W') {
+						//caso in cui il nemico è a nord e mi muovo in alto
+						if (Attacco(ent, SkillAttack)) {
+						}
+						clockAttack.restart();
 					}
 				}
-				else {
-					
-					if (this->S && resolvery > 0) {
-						isblockedS = true;
-					}
-					else if (this->W && resolvery <= 0) {//altrimenti verso l'alto.
-						isblockedW = true;
-					}
-					else {
-						//situazione strana + diagonale
-					}
+				
+
+				
 			}
+			
+			if (Interacting) {
+			Giocatore* x = dynamic_cast<Giocatore*>(this);
+				if (x != nullptr) {
+					x->Interazione(*ent); //attivo l'interazione di questo tipo di entità (player)
+				}
+			}
+
+			//casi ulteriori (blocco movimento):
+			if (this->D && segmentx > 0 && Touchable(segmenty)){ 
+				//l'entità sta collidendo alla mia destra e mi muovo a destra (ed è toccabile).
+				isblockedD = true;
+			}
+			else if (this->A && segmentx <= 0 && Touchable(segmenty)) {
+				//l'entità sta collidendo alla mia sinistra e mi muovo a sinistra (ed è toccabile).
+				isblockedA = true;
+			}
+			else {
+			}
+				
+					
+			if (this->S && segmenty > 0) { 
+				//verso basso
+				isblockedS = true;
+			}
+			else if (this->W && segmenty <= 0) {
+				//altrimenti verso l'alto.
+				isblockedW = true;
+			}
+			else {
+			}
+
+
+				
+			
 		}
 		entitiescollided.clear();
 	}
 
+}
+
+void Entita::Update(sf::Time const& dt) {
+	animpg.update(dt);
+	updateCollRect();
+	
+	if (GetVitalita() < GetMaxVitalita())
+		RegenHitPoints();
+
+	Giocatore* ps = dynamic_cast<Giocatore*>(this);
+	if (ps != nullptr) {
+
+		if(ps->GetStamina() < ps->GetMaxStamina())
+			ps->RegenStamina();
+	}
+}
+Animator* Entita::GetAnimpg()
+{
+	return &animpg;
+}
+std::vector<AnimSet> Entita::GetAnimations()
+{
+	return animations;
+}
+Inventario* Entita::GetInventario()
+{
+	return &inventario;
+}
+void Entita::SetMaxVitalita(int vit) {
+	this->maxvitalita = vit;
+}
+int Entita::GetMaxVitalita() {
+	return this->maxvitalita;
+}
+
+void Entita::SetLastDirection(char newDirection) {
+	this->lastDirection = newDirection;
+}
+char Entita::GetLastDirection() {
+	return lastDirection;
+}
+std::string Entita::GetNome() {
+	return nome;
+}
+void Entita::SetNome(std::string newnome) {
+	this->nome = newnome;
+}
+int Entita::GetVitalita() {
+	return this->vitalita;
+}
+void Entita::SetVitalita(int vit) {
+	this->vitalita = vit;
+}
+float  Entita::GetVelocita() {
+	return velocita/100;
+}
+void Entita::SetVelocita(float vel) {
+	velocita = vel;
+}
+int Entita::GetForza() {
+	return forza;
+}
+void Entita::SetForza(int const& forza) {
+	this->forza = forza;
+}
+int Entita::GetLivello() {
+	return livello;
+}
+void Entita::SetLivello(int const& liv) {
+	livello = liv;
+}
+
+sf::Vector2f Entita::GetPosition() {
+	return position;
+}
+void Entita::SetPosition(const sf::Vector2f const& newpos) {
+	this->position = newpos;
+}
+sf::Sprite& Entita::GetSprite() {
+	return sprite;
+}
+void Entita::SetSprite(sf::Sprite sprite) {
+	this->sprite = sprite;
+}
+sf::FloatRect Entita::GetCollRect() {
+	return rectColl;
+}
+void Entita::setContext(SharedContext& context) {
+	this->context = context;
+}
+unsigned int Entita::GetId() {
+	return id;
+}
+void Entita::setId(unsigned int id1) {
+	id = id1;
+}
+
+void Entita::SetEntityType(const std::string& newType)
+{
+	entitytype = newType;
+}
+
+std::string Entita::GetEntityType()
+{
+	return entitytype;
+}
+
+
+
+bool Entita::Touchable(float n1) {
+	float x = abs(n1);
+
+	if (x < 0.84)
+		return true;
+
+	return false;
+}
+
+void Entita::PreparaAttacco(int isSkill) {
+	// attivo l'attacco e setto quale skill è
+	SkillAttack = isSkill;
+	Attacking = true;
+}
+void Entita::PreparaInterazione() {
+	// setto l'interazione a true
+	Interacting = true;
+}
+
+bool Entita::isBlockedW()
+{
+	return isblockedW;
+}
+
+bool Entita::isBlockedS()
+{
+	return isblockedS;
+}
+
+bool Entita::isBlockedA()
+{
+	return isblockedA;
+}
+
+bool Entita::isBlockedD()
+{
+	return isblockedD;
+}
+
+void Entita::RegenHitPoints() {
+	sf::Time regenHP= clockVitalita.getElapsedTime();
+	if (regenHP.asSeconds() >= speedHitPointsRecovery) {
+		regenHP = clockVitalita.restart();
+		vitalita += 1;
+	}
+}
+void Entita::setMap(const std::string& map) {
+	this->map = map;
+}
+std::string Entita::getMap() {
+	return this->map;
+}
+void Entita::DrawEquipment() {
+	inventario.DrawItems();
 }
--- ConsoleApplication1/Entita.h
+++ ConsoleApplication1/Entita.h
@@ -5,120 +5,123 @@
 #include "SharedContext.h"
 #include "Collisione.h"
 #include "Animator.h"
-enum class EntityType { Enemy, MiniBoss, Player, Shopkeeper, Guard };
+#include "Inventario.h"
+enum class EntityType { Enemy, MiniBoss, Player, NPC };
+enum class NpcType { Guard, Shopkeeper };
+enum class EnemyType { Skeleton, Crystal, Bandit};
 enum class EntityState { Idle, Walking, Attacking, Dying };
 using AnimSet = Animator::Animation;
 class Entita {
 public:
-	virtual void Movimento(float x, float y) = 0;
-	virtual void Attacco(bool isSkill) = 0;
-
-	bool isblockedW;
-	bool isblockedS;
-	bool isblockedA;
-	bool isblockedD;
+	virtual void Movimento(const float &x, const float &y) = 0;
+	virtual bool Attacco(Entita* ent, int isSkill) = 0;
+	void PreparaAttacco(int isSkill);
+	void PreparaInterazione();
 	
-	std::vector<AnimSet> animations;
-	std::vector<Entita*> entitiescollided;
-	Animator animpg;
+	bool isBlockedW();
+	bool isBlockedS();
+	bool isBlockedA();
+	bool isBlockedD();
 
-	std::string GetNome() {
-		return this->nome;
-	}
-	void SetNome(std::string newnome) {
-		this->nome = newnome;
-	}
-	int GetVitalita() {
-		return this->vitalita;
-	}
-	void SetVitalita(int vit) {
-		this->vitalita = vit;
-	}
-	float  GetVelocita() {
-		return this->velocita;
-	}
-	void SetVelocita(float vel) {
-		this->velocita = vel;
-	}
-	int GetForza() {
-		return this->forza;
-	}
-	void SetForza(int const& forza) {
-		this->forza = forza;
-	}
-	int GetLivello() {
-		return this->livello;
-	}
-	void SetLivello(int const& liv) {
-		this->livello = liv;
-	}
-	sf::Vector2f GetPosition() {
-		return this->position;
-	}
-	void SetPosition(sf::Vector2f const& newpos) {
-		this->position = newpos;
-	}
-	sf::Sprite& GetSprite() {
-		return this->sprite;
-	}
-	void SetSprite(sf::Sprite sprite) {
-		this->sprite = sprite;
-	}
-	sf::FloatRect GetCollRect() {
-		return rectColl;
-	}
-	void setContext(SharedContext* context) {
-		this->context = context;
-	}
-	unsigned int GetId() {
-		return id;
-	}
-	void setId(unsigned int id1) {
-		id = id1;
-	}
+	void Update(sf::Time const& dt);
+	std::string GetNome();
+	void SetNome(std::string newnome);
+	int GetVitalita();
+	void SetVitalita(int vit);
+	int GetMaxVitalita();
+	void SetMaxVitalita(int vit);
+	float  GetVelocita();
+	void SetVelocita(float vel);
+	int GetForza();
+	void SetForza(int const& forza);
+	int GetLivello();
+	void SetLivello(int const& liv);
+	Animator* GetAnimpg();
+	std::vector<AnimSet> GetAnimations();
+	Inventario* GetInventario();
+	void SetLastDirection(char newDirection);
+	char GetLastDirection();
+	sf::Vector2f GetPosition();
+	void SetPosition(const sf::Vector2f const& newpos);
+	sf::Sprite& GetSprite();
+	void SetSprite(sf::Sprite sprite);
+	sf::FloatRect GetCollRect();
+	void setContext(SharedContext& context);
+	unsigned int GetId();
+	void setId(unsigned int id1);
+	void SetEntityType(const std::string& newType);
+	std::string GetEntityType();
 	void setSpawnPoint(sf::Vector2f pos);
+	void setMap(const std::string& map);
+	std::string getMap();
 
-	void CheckCollisions(SharedContext* context);
+	void CheckCollisions(/*SharedContext& context*/);
 	// è una funzione che mi serve a determinare se ci sono state collissioni e di che tipo
-	void CollisionEntity(Entita* ent, SharedContext* context);
-	void ResolveCollisions(SharedContext* context); // è una funzione che mi server per determinare cosa fare per le determinate collissioni, che decisioni prendere.
-	void ResolveCollisionEntity(SharedContext* context);
-	Entita(std::string nome, int forza, int vitalita, int livello, float velocita, sf::Vector2f pos, unsigned int id, SharedContext* context, std::string entityType) : entitytype(entityType), id(id), context(context), vitalita(vitalita), nome(nome), forza(forza), velocita(velocita), livello(livello), sprite(sf::Sprite()), isblockedA(false), isblockedD(false), isblockedS(false), isblockedW(false), A(false),D(false),W(false),S(false),animpg(sprite) {
-		SetSizeRC(20, 26);
-		setSpawnPoint(pos);
+	void CollisionEntity(Entita* ent/*, SharedContext* context*/);
+	void ResolveCollisions(/*SharedContext* context*/); // è una funzione che mi server per determinare cosa fare per le determinate collissioni, che decisioni prendere.
+	void ResolveCollisionEntity(/*SharedContext* context*/);
+	Entita(const std::string &nome, const int &forza, const int &vitalita, const int &livello, const float &velocita, const sf::Vector2f &pos, unsigned int id, SharedContext& context, const std::string const &entityType) : entitytype(entityType), id(id), context(context), vitalita(vitalita),maxvitalita(vitalita), nome(nome), forza(forza), velocita(velocita), livello(livello), sprite(sf::Sprite()), isblockedA(false), isblockedD(false), isblockedS(false), isblockedW(false), A(false),D(false),W(false),S(false),Attacking(false),Interacting(false), lastDirection('S'),animpg(sprite),sizeRC(sf::Vector2f(20,26)), inventario(context) {
+		
+		updateCollRect();
 	}
 	virtual ~Entita() {}
-protected:
+private:
+	std::vector<Entita*> entitiescollided;
 
 	void SetSizeRC(const float& x, const float& y) {
 		//funzione per settare la nuova area di collisione attorno all'entità
 		sizeRC = sf::Vector2f(x, y);
 		updateCollRect();
-	}
+	}	
+	sf::FloatRect rectColl;
+	sf::Vector2f sizeRC; //size del Rect di Collisione dell'entità
+	Collisions colls;
+protected:
 	void updateCollRect() {
 		// è necessario aggiornare il rectColl, a causa del fatto che l'entità si muove.
 		// in altre parole cambierà position.x e position.y
 		rectColl = sf::FloatRect(position.x, position.y, sizeRC.x, sizeRC.y);
 	}
+	sf::Vector2f position;
+	sf::Vector2f oldPosition;
+	sf::Sprite sprite;
+	std::vector<AnimSet> animations;
+	Animator animpg;
+	
+	Inventario inventario;
+	
+	bool isblockedW;
+	bool isblockedS;
+	bool isblockedA;
+	bool isblockedD;
+	
+	bool Touchable(float n1);
+	void DrawEquipment();
+	void RegenHitPoints();
 
-
-	std::string nome;
-	int vitalita;
-	float velocita;
-	std::string entitytype;
-	int forza;
-	int livello;
+	// Controlli
 	bool W;
 	bool S;
 	bool A;
 	bool D;
-	// TODO salva mappa d'appartenenza.
-	sf::Vector2f position;
-	sf::Vector2f oldPosition;
-	sf::FloatRect rectColl;
-	sf::Vector2f sizeRC; //size del Rect di Collisione dell'entità
-	Collisions colls;
-	sf::Sprite sprite;
+	char lastDirection;
+	bool Attacking;
+	int SkillAttack;
+	bool Interacting;
+
+	sf::Clock clockAttack;
+	sf::Clock clockVitalita;
+	float speedHitPointsRecovery = 2.5f;
+	float cooldownAutoAttack = 0.5f;
+	
+	std::string nome;
+	int vitalita;
+	int maxvitalita;
+	float velocita;
+	std::string entitytype;
+	int forza;
+	int livello;
 	unsigned int id;
-	SharedContext* context;
+	std::string map;
+	SharedContext& context;
 };
