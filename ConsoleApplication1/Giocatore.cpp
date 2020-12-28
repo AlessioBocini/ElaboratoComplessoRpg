bc2294acbe8ee693f47c42081c735d8097f8d561
AlessioB <38136763+AlessioBocini@users.noreply.github.com>
2020-12-28 13:19:17 +0100

    Giocatore - Commit
    + Interazione
    + Attacco

--- ConsoleApplication1/Giocatore.cpp
+++ ConsoleApplication1/Giocatore.cpp
@@ -5,35 +5,113 @@
 #include "Mondo.h"
 
 
-void Giocatore::Attacco(bool isSkill) {
+bool Giocatore::Attacco(Entita *ent, int Skill) {
+	bool executed = false;
+
+	switch (Skill)
+	{
+	case -1: {
+		if ((stamina - 10) >= 0) {
+			stamina -= 10;
+			ent->SetVitalita(ent->GetVitalita()-(forza/100));
+			executed = true;
+		}
+		
+		break;
+	}
+		
+	default:
+		break;
+	}
+	Attacking = false;
+	return executed;
 }
 
 
-void Giocatore::Movimento(float x, float y) {
+void Giocatore::Movimento(const float &x, const float &y) {
 	oldPosition = position;
 	position += sf::Vector2f(x, y);
 	A = false, D = false, S = false, W = false;
-	if (x > 0) 
+	if (x > 0) {
 		D = true;
-	else if(x<0)
+		lastDirection = 'D';
+	}
+	else if (x < 0) {
 		A = true;
+		lastDirection = 'A';
+	}
+		
 
-	if (y > 0)
+	if (y > 0) {
 		S = true;
-	else if(y<0)
+		lastDirection = 'S';
+	}
+	else if (y < 0) {
 		W = true;
+		lastDirection = 'W';
+	}
+		
 
 	sprite.move(x, y);
 	updateCollRect(); //aggiorna le informazioni sulle collisioni
 }
 
-bool Giocatore::EquipArmor(Armatura const& obj) {
+bool Giocatore::EquipArmor(const Armatura const& obj) {
 	return true;
 }
-bool Giocatore::EquipWeapon(Arma const& obj) {
+bool Giocatore::EquipWeapon(const Arma const& obj) {
 	return true;
 }
-bool Giocatore::Interazione(Entita const& ent) {
+bool Giocatore::Interazione(Entita & ent) {
+	std::cout << "sto parlando con : " << ent.GetNome() << std::endl;
+	Interacting = false;
 	return true;
 }
 
+int Giocatore::GetStamina()
+{
+	return stamina;
+}
+
+void Giocatore::SetStamina(int stam)
+{
+	this->stamina = stam;
+}
+
+int Giocatore::GetMaxStamina()
+{
+	return maxstamina;
+}
+
+void Giocatore::SetMaxStamina(int stam)
+{
+	this->maxstamina = stam;
+}
+
+float Giocatore::GetExp()
+{
+	return exp;
+}
+
+void Giocatore::SetExp(float newexp)
+{
+	exp = newexp;
+}
+
+void Giocatore::ToggleInventory()
+{
+	inventario.ToggleVisibility();
+}
+
+void Giocatore::RegenStamina() {
+		sf::Time regenStamina = clockStamina.getElapsedTime();
+	if (regenStamina.asSeconds() >= speedStaminaRecovery) {
+		regenStamina = clockStamina.restart();
+		stamina += 1;
+	}
+}
+
+std::vector<Quickslot>* Giocatore::GetQuickslots()
+{
+	return &quickslots;
+}
--- ConsoleApplication1/Giocatore.h
+++ ConsoleApplication1/Giocatore.h
@@ -6,37 +6,37 @@
 #include "Arma.h"
 #include "Armatura.h"
 #include <iostream>
+#include "Quickslot.h"
 
 class Giocatore : public Entita {
 private:
-	Inventario inventario;
-	std::vector<Skill> skills;
+	
+	std::vector<Quickslot> quickslots; 
 	int stamina;
+	int maxstamina;
+
+	float speedStaminaRecovery = 2.0f;
+	sf::Clock clockStamina;
+
 	float exp;
 public:
-	void Attacco(bool isSkill) override;
-	void Movimento(float x, float y) override;
+	bool Attacco(Entita*ent, int isSkill) override;
+	void Movimento(const float &x, const float &y) override;
 
-	bool EquipWeapon(Arma const& obj);
-	bool EquipArmor(Armatura const& obj);
-	bool Interazione(Entita const& ent);
-	int GetStamina() {
-		return this->stamina;
-	}
-	void SetStamina(int stam) {
-		this->stamina = stam;
-	}
-	float GetExp() {
-		return this->exp;
-	}
-	void SetExp(float newexp) {
-		this->exp = newexp;
-	}
-	Giocatore(std::string const& nome, int forza, int  vitalita, int stamina, int livello, float velocita, sf::Vector2f pos, float exp, unsigned int id, SharedContext* context, std::string entitytype) :Entita(nome, forza, vitalita, livello, velocita, pos, id, context, entitytype) {
-		this->SetStamina(stamina);
-		inventario = Inventario();
-		skills = std::vector<Skill>();
-		this->SetExp(exp);
-	}
-	Giocatore(std::string const& nome, sf::Vector2f pos) : Giocatore(nome, 100, 100, 100, 1, 1, pos, 0, 1, nullptr, "Player") {}
-};
+	bool EquipWeapon(const Arma const& obj);
+	bool EquipArmor(const Armatura const& obj);
+	bool Interazione(Entita & ent);
+	int GetStamina(); 
+	void SetStamina(int stam);
+	int GetMaxStamina();
+	void SetMaxStamina(int stam);
+	float GetExp();
+	void SetExp(float newexp);
+	void ToggleInventory();
+	void RegenStamina();
+	std::vector<Quickslot>* GetQuickslots();
+	Giocatore(std::string const& nome, int forza, int  vitalita, int stamina, int livello, float velocita, const sf::Vector2f &pos, float exp, unsigned int id, SharedContext& context, const std::string const &entitytype) :Entita(nome, forza, vitalita, livello, velocita, pos, id, context, entitytype),stamina(stamina),maxstamina(stamina) ,exp(exp) ,quickslots(std::vector<Quickslot>()){}
+	//Giocatore(std::string const& nome, const sf::Vector2f &pos) : Giocatore(nome, 100, 100, 100, 1, 100, pos, 0, 1, nullptr, "Player") {}
+	Giocatore(std::string const& nome, const sf::Vector2f &pos,SharedContext& context) : Giocatore(nome, 100, 100, 100, 1, 100, pos, 0, 1, context, "Player") {}
+	~Giocatore() override{}
+}; 
