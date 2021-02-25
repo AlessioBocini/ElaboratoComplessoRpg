0d1eb6436e05488037c1f221697080dd594590bf
AlessioB <38136763+AlessioBocini@users.noreply.github.com>
2021-02-25 18:17:28 +0100

    Npc - Commit
    + Aggiornata interazione, nel caso in cui l'NPC è di tipo Shopkeeper vende al costo di 2 denari un'arma.

--- ConsoleApplication1/NPC.h
+++ ConsoleApplication1/NPC.h
@@ -3,17 +3,16 @@
 
 class Npc : public Entita {
 public:
-	Npc(std::string nome, int forza, int vitalita, int livello, float velocita, sf::Vector2f pos, unsigned int id, SharedContext* context, std::string entityType) : Entita(nome, forza, vitalita, livello, velocita, pos, id, context, entityType) {}
-	~Npc() {}
+	Npc(std::string nome, int forza, int vitalita, int livello, float velocita, sf::Vector2f pos, unsigned int id, SharedContext& context, std::string entityType) : Entita(nome, forza, vitalita, livello, velocita, pos, id, context, entityType),typeofNpc(-1) {}
+	Npc(std::string nome, int forza, int vitalita, int livello, float velocita, sf::Vector2f pos, unsigned int id, SharedContext& context) : Npc(nome, forza, vitalita, livello, velocita, pos, id, context, ""){}
+	~Npc() override {}
 
-	int getTypeOfNpc();
-	void Attacco(bool isSkill) override;
-	void Movimento(float x, float y) override;
+	int getTypeOfNpc() const;
+	bool Attacco(Entita*ent,int isSkill) override;
+	void Movimento(const float &x, const float &y) override;
 	void setTypeOfNpc(const int &type);
-	std::string getMap();
-	void setMap(const std::string& map);
+	bool Interazione(Entita& ent) override;
 
 private:
 	int typeofNpc;
-	std::string map;
 };
--- ConsoleApplication1/Npc.cpp
+++ ConsoleApplication1/Npc.cpp
@@ -1,20 +1,41 @@
 #include "NPC.h"
+#include "Giocatore.h"
 
-void Npc::setMap(const std::string& map) {
-	this->map = map;
-}
 void Npc::setTypeOfNpc(const int& type) {
 	this->typeofNpc = type;
 }
-std::string Npc::getMap() {
-	return this->map;
+
+bool Npc::Interazione(Entita& ent)
+{
+
+	if (clockInterazione.getElapsedTime().asSeconds() < cooldownInterazione) {
+		return false;
+	}
+	ent.GetInventario()->GetDenaro();
+
+	if ((int(NpcType::Shopkeeper)) != this->typeofNpc) {
+		return false;
+	}
+
+	int costo = 2;
+	auto inv = ent.GetInventario();
+	if (inv->GetDenaro() >= costo) {
+		inv->DecreaseDenaro(costo);
+		Equipaggiamento eq = Equipaggiamento("Destroyer", "assets/images/pack/Weapons/weaponicon.png", "assets/images/pack/Weapons/weaponiconactivate.png");
+		inv->AggiungiOggetto(eq,50);
+		std::cout << "Hai speso " << costo << " denari, hai acquistato " << eq.GetNomeEquip() << std::endl;
+	}
+	clockInterazione.restart();
+	return true;
 }
-int Npc::getTypeOfNpc() {
+
+int Npc::getTypeOfNpc() const{
 	return this->typeofNpc;
 }
 
-void Npc::Attacco(bool isSkill) {
+bool Npc::Attacco(Entita*ent,int isSkill) {
+	return 0;
 }
-void Npc::Movimento(float x, float y) {
+void Npc::Movimento(const float &x, const float &y) {
 
 }
